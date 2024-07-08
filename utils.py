96fd32271fc659da63579de0b2ac92d507a9a227
hervivas <71046365+hervivas@users.noreply.github.com>
2024-07-08 10:14:16 -0300

    Update utils.py
    Added the load_data function.

--- utils.py
+++ utils.py
@@ -53,3 +53,25 @@ def evaluate_model(model, X_test, y_test):
     print("Precision:", precision_best)
     print("Recall:", recall_best)
     print("F1-score:", f1_best)
+
+def load_data(dataPath: str) -> tuple:
+    base_train = pl.read_csv(dataPath + "csv_files/train/train_base.csv")
+    person_1_train = pl.read_csv(dataPath + 'csv_files/train/train_person_1.csv').pipe(set_table_dtypes)
+    applprev_train = pl.concat(
+        [
+            pl.read_csv(dataPath + 'csv_files/train/train_applprev_1_0.csv').pipe(set_table_dtypes),
+            pl.read_csv(dataPath + 'csv_files/train/train_applprev_1_1.csv').pipe(set_table_dtypes),
+        ],
+        how="vertical_relaxed",
+    )
+    static_train = pl.concat(
+        [
+            pl.read_csv(dataPath + "csv_files/train/train_static_0_0.csv").pipe(set_table_dtypes),
+            pl.read_csv(dataPath + "csv_files/train/train_static_0_1.csv").pipe(set_table_dtypes),
+        ],
+        how="vertical_relaxed",
+    )
+    static_cb_train = pl.read_csv(dataPath + "csv_files/train/train_static_cb_0.csv").pipe(set_table_dtypes)
+    credit_bureau_b_2_train = pl.read_csv(dataPath + 'csv_files/train/train_credit_bureau_b_2.csv').pipe(set_table_dtypes)
+
+    return base_train, person_1_train, applprev_train, static_train, static_cb_train, credit_bureau_b_2_train
